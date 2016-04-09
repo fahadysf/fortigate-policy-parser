489924a0085e6d6d852838e049c751036f59e5ff
fahadysf <fahadysf@gmail.com>
2016-04-09 12:46:17 +0300

    Adding Python 3.x compatibility.

--- fortigate-policy-parser.py
+++ fortigate-policy-parser.py
@@ -8,25 +8,25 @@ import openpyxl
 pp = pprint.PrettyPrinter(indent=2)
 
 parser = argparse.ArgumentParser(description="Process Fortigate configuration \
-	and create communication matrix excel sheet.")
+    and create communication matrix excel sheet.")
 parser.add_argument('-f', action='store',
                    metavar='<configuration-file>',
                    help='path to configuration file',
                    required=True)
 
 parser.add_argument('-o', action='store',
-					metavar='<output-file>.xls',
-					help='path to configuration file',
-					required=False)
+                    metavar='<output-file>.xls',
+                    help='path to configuration file',
+                    required=False)
 
 args = parser.parse_args()
 CONFIGFILE = vars(args)['f']
-print ("Parsing Configuration File: %s" % CONFIGFILE)
+print(("Parsing Configuration File: %s" % CONFIGFILE))
 
 try:
-	fullconfigstr = open(CONFIGFILE, 'r').read()
+    fullconfigstr = open(CONFIGFILE, 'r').read()
 except:
-	print("Error reading config file: %s" % CONFIGFILE)
+    print(("Error reading config file: %s" % CONFIGFILE))
 
 fullconfiglines = fullconfigstr.splitlines()
 
@@ -36,18 +36,18 @@ allpolicies = allpolicies[:allpolicies.index('end')]
 policydict = dict()
 
 for line in allpolicies:
-	try:
-		if line.strip().startswith('edit'):
-			policyid = re.match(r'edit (\d*)', line.strip()).groups()[0]
-			policydict[policyid] = dict()
-		elif line.strip() != 'next' and line.strip().startswith('set'):
-			key, val = re.match(r'^set (\S*) (.+)$', line.strip()).groups()
-			policydict[policyid][key] = val
-	except:
-		print "Error on line: %s" % line
-		raise
+    try:
+        if line.strip().startswith('edit'):
+            policyid = re.match(r'edit (\d*)', line.strip()).groups()[0]
+            policydict[policyid] = dict()
+        elif line.strip() != 'next' and line.strip().startswith('set'):
+            key, val = re.match(r'^set (\S*) (.+)$', line.strip()).groups()
+            policydict[policyid][key] = val
+    except:
+        print(("Error on line: %s" % line))
+        raise
 
-print ("Total policies: %d" % len(policydict.keys()))
+print(("Total policies: %d" % len(list(policydict.keys()))))
 
 if 'o' in vars(args):
     wb = openpyxl.Workbook()
@@ -56,7 +56,7 @@ if 'o' in vars(args):
     row = 2
     columns = list()
     columns.append('id')
-    for pid in policydict.keys():
+    for pid in list(policydict.keys()):
         for key in policydict[pid]:
             if not key in columns:
                 columns.append(key)
@@ -66,7 +66,7 @@ if 'o' in vars(args):
                     policydict[pid][key] = policydict[pid][key].replace('_', '-')
                 policydict[pid][key] = policydict[pid][key].split(',')
 
-    for pid in policydict.keys():
+    for pid in list(policydict.keys()):
         sheet.cell(row=row, column=columns.index('id') + 1).value = pid
         for key in policydict[pid]:
             if type(policydict[pid][key]) == type(list()):
